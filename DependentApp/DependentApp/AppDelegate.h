f1f84dcc42967978d00966ba90989210baad354c
juniechu <juniechu@qq.com>
2013-05-31 18:39:44 +0800

    Dependent App init

---
+++ DependentApp/DependentApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DependentApp
+//
+//  Created by 朱 俊杰 on 13-5-28.
+//  Copyright (c) 2013年 朱 俊杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DependentApp/DependentApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DependentApp
+//
+//  Created by 朱 俊杰 on 13-5-28.
+//  Copyright (c) 2013年 朱 俊杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
